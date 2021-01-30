0379f6d503345aa6a66f3bacaf811ff1fe35f662
JosephBashful <63520369+JosephBashful@users.noreply.github.com>
2021-01-30 13:42:32 +0100

    Player physics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -42,18 +42,6 @@ public class PlayerMovement : MonoBehaviour
             rb.drag = 1;
         }
     }
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-        Debug.Log("EXIT");
-        InVolo = true;
-	}
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("ENTER");
-        InVolo = false;
-    }
     /*
     private void OnCollisionExit2D(Collision2D collision)
 	{
