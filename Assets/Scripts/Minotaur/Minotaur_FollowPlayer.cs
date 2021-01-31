ada31b366ee95d2c942bda18fa9af0b6ffcf7d4c
Axelpass <axelpas@hotmail.it>
2021-01-31 10:01:05 +0100

    Minotaur rocks mechanic

--- Assets/Scripts/Minotaur/Minotaur_FollowPlayer.cs
+++ Assets/Scripts/Minotaur/Minotaur_FollowPlayer.cs
@@ -9,10 +9,11 @@ public class Minotaur_FollowPlayer : MonoBehaviour
     private Player_MinotaurFollowPoints Player_FollowPointsScript;
     private Animator Minotaur_Anims_Animator;
     private Transform Minotaur_BodyContainer;
+    private Minotaur_FallCheck Minotaur_PlatformCheckScript;
 
     [HideInInspector] public bool Minotaur_FoPl_IsJumping = false;
 
-    [HideInInspector] public GameObject Minotaur_FoPl_PointToReach;
+    /*[HideInInspector]*/ public GameObject Minotaur_FoPl_PointToReach;
 
     [HideInInspector] public float Minotaur_FoPl_LerpTimer = 0.0f;
     private float Minotaur_FoPl_ActualLerpSpeed = 8f;
@@ -27,7 +28,10 @@ public class Minotaur_FollowPlayer : MonoBehaviour
     public float Minotaur_FoPl_CoverDistance_NewLerpSpeed = 12.0f;
     public float Minotaur_FoPl_CoverDistance_PathDistanceForBackToNormalSpeed = 3.0f;
 
-    private Vector3 Minotaur_LastFramePosition;
+    [HideInInspector] public Vector3 Minotaur_LastFramePosition;
+
+    private float Minotaur_FoPl_SafetyCheck_OnGround_Timer = 1.0f;
+    private float Minotaur_FoPl_SafetyCheck_OnGround_Timer_Actual = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -43,16 +47,23 @@ public class Minotaur_FollowPlayer : MonoBehaviour
 
         Minotaur_Anims_Animator = this.gameObject.GetComponent<Animator>();
 
-        Minotaur_BodyContainer = transform.Find("Body_Container");
+        Minotaur_BodyContainer = this.gameObject.transform;//transform.Find("Body_Container");
+
+        Minotaur_PlatformCheckScript = transform.GetChild(0).GetChild(0).GetChild(0).gameObject.GetComponent<Minotaur_FallCheck>();
+
+        this.gameObject.GetComponent<Minotaur_TeleportToPlayer>().Player = Player;
+        this.gameObject.GetComponent<Minotaur_TeleportToPlayer>().FollowPlayerScript = this;
 
         Minotaur_LastFramePosition = transform.position;
+
+        Minotaur_FoPl_SafetyCheck_OnGround_Timer_Actual = Minotaur_FoPl_SafetyCheck_OnGround_Timer;
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Debug.Log(Minotaur_FoPl_IsFollowing);
+        Debug.Log(Minotaur_FoPl_IsFollowing + "; " + Minotaur_FoPl_IsJumping);// + "; " + Minotaur_FoPl_PointToReach.transform.position);
         if (Minotaur_FoPl_PointToReach != null)
         {
 
@@ -77,18 +88,23 @@ public class Minotaur_FollowPlayer : MonoBehaviour
 
                     Minotaur_Anims_Animator.SetBool("IsWalking", true);
                     transform.position = Vector3.Lerp(Minotaur_FoPl_LerpStartPosition, Minotaur_FoPl_PointToReach.transform.position, Minotaur_FoPl_LerpTimer);
+                    //Debug.Log(Minotaur_FoPl_LerpStartPosition + "; " + Minotaur_FoPl_PointToReach.transform.position + "; " + Minotaur_FoPl_LerpTimer);
 
                     float lastFrameMovementLength = Vector3.Distance(transform.position, Minotaur_LastFramePosition);
+                    //Debug.Log(lastFrameMovementLength + "; " + Player_FollowPointsScript.Player_FollowPoints_PathLength);
                     Player_FollowPointsScript.Player_FollowPoints_PathLength -= lastFrameMovementLength;
+                    //Debug.Log(Player_FollowPointsScript.Player_FollowPoints_PathLength);
 
                     if (Player_FollowPointsScript.Player_FollowPoints_PathLength <= Minotaur_FoPl_DistanceForStop)
                     {
+                        //Debug.Log("1");
                         Minotaur_FoPl_IsFollowing = false;
                         Minotaur_Anims_Animator.SetBool("IsWalking", false);
                     }
                     else
                     {
 
+                        //Debug.Log("2");
                         /*float lastFrameMovementLength = Vector3.Distance(transform.position, Minotaur_LastFramePosition);
                         Player_FollowPointsScript.Player_FollowPoints_PathLength -= lastFrameMovementLength;*/
 
@@ -167,6 +183,7 @@ public class Minotaur_FollowPlayer : MonoBehaviour
             else
             {
 
+                Debug.Log("!!!!!");
                 Minotaur_Anims_Animator.SetBool("IsWalking", true);
                 //Minotaur_Anims_Animator.SetBool("IsJumping", true);
                 transform.position = Vector3.Lerp(Minotaur_FoPl_LerpStartPosition, Minotaur_FoPl_PointToReach.transform.position, Minotaur_FoPl_LerpTimer);
@@ -184,7 +201,7 @@ public class Minotaur_FollowPlayer : MonoBehaviour
                 }
                 else
                 {
-
+                    Debug.Log("????");
                     GameObject nextPoint = Minotaur_FoPl_PointToReach.GetComponent<Minotaur_FollowPoint>().FollowPoint_Next;
 
                     if (Minotaur_FoPl_PointToReach.GetComponent<Minotaur_FollowPoint>().FollowPoint_ToggleJump)
@@ -229,6 +246,22 @@ public class Minotaur_FollowPlayer : MonoBehaviour
             }
 
         }
+
+
+        Minotaur_FoPl_SafetyCheck_OnGround_Timer_Actual -= Time.deltaTime;
+        if (Minotaur_FoPl_SafetyCheck_OnGround_Timer_Actual <= 0.0f)
+        {
+
+            if (!Player.GetComponent<PlayerMovement>().InVolo && Minotaur_PlatformCheckScript.IsGrounded)
+            {
+                Minotaur_FoPl_IsJumping = false;
+            }
+            //Minotaur_FoPl_IsJumping = Minotaur_PlatformCheckScript.IsGrounded; //Player.GetComponent<PlayerMovement>().InVolo;
+
+            Minotaur_FoPl_SafetyCheck_OnGround_Timer_Actual = Minotaur_FoPl_SafetyCheck_OnGround_Timer;
+
+        }
+
 
     }
 
